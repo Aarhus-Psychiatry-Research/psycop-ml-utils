51c5c847274f0d50c279571b44d39193a1bc297c
Martin Bernstorff <martinbernstorff@gmail.com>
2022-04-20 10:16:11 +0200

    Same

--- src/timeseriesflattener/resolve_multiple_functions.py
+++ src/timeseriesflattener/resolve_multiple_functions.py
@@ -16,6 +16,19 @@ def get_latest_val_in_group(grouped_df: DataFrame) -> DataFrame:
     return grouped_df.last()
 
 
+@resolve_fns.register("earliest")
+def get_earliest_val_in_group(grouped_df: DataFrame) -> DataFrame:
+    """Get the earliest value.
+
+    Args:
+        grouped_df (DataFrame): A dataframe sorted by descending timestamp, grouped by citizen.
+
+    Returns:
+        DataFrame: Dataframe with only the earliest value in each group.
+    """
+    return grouped_df.first()
+
+
 @resolve_fns.register("max")
 def get_max_in_group(grouped_df: DataFrame) -> DataFrame:
     return grouped_df.max()
@@ -29,19 +42,6 @@ def get_min_in_group(grouped_df: DataFrame) -> DataFrame:
 @resolve_fns.register("average")
 def get_mean_in_group(grouped_df: DataFrame) -> DataFrame:
     return grouped_df.mean()
-
-
-@resolve_fns.register("earliest")
-def get_earliest_val_in_group(grouped_df: DataFrame) -> DataFrame:
-    """Get the earliest value.
-
-    Args:
-        grouped_df (DataFrame): A dataframe sorted by descending timestamp, grouped by citizen.
-
-    Returns:
-        DataFrame: Dataframe with only the earliest value in each group.
-    """
-    return grouped_df.first()
 
 
 @resolve_fns.register("sum")
