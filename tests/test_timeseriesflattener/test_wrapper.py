fb69d82e5f2b744ff0fb25550d7178ce13c79e18
Martin Bernstorff <martinbernstorff@gmail.com>
2022-04-22 14:25:51 +0200

    Add test of input ordering

--- tests/test_timeseriesflattener/test_wrapper.py
+++ tests/test_timeseriesflattener/test_wrapper.py
@@ -1,3 +1,4 @@
+from pandas.testing import assert_frame_equal
 from timeseriesflattener.create_feature_combinations import create_feature_combinations
 from timeseriesflattener.flattened_dataset import FlattenedDataset
 from timeseriesflattener.resolve_multiple_functions import get_max_in_group
@@ -20,6 +21,7 @@ def test_generate_two_features_from_dict():
     expected_df_str = """dw_ek_borger,timestamp,val_within_1_days,val_within_2_days,val_within_3_days,val_within_4_days
                         1,2021-12-31 00:00:00,1,2,2,2                      
     """
+
     prediction_times_df = str_to_df(prediction_times_str)
     event_times_df = str_to_df(event_times_str)
     expected_df = str_to_df(expected_df_str)
@@ -45,8 +47,98 @@ def test_generate_two_features_from_dict():
 
     flattened_dataset.add_predictors_from_list_of_argument_dictionaries(
         predictor_list=predictor_list,
-        predictor_dfs={"event_times_df": event_times_df},
+        predictor_dfs_dict={"event_times_df": event_times_df},
         resolve_multiple_fn_dict={"max": get_max_in_group},
     )
 
     assert flattened_dataset.df.equals(expected_df)
+
+
+def test_output_independent_of_order_of_input():
+    """Test generation of features from a dictionary."""
+
+    prediction_times_str = """dw_ek_borger,timestamp,
+                            1,2021-12-31 00:00:00
+                            1,2021-12-31 00:00:01
+                            2,2021-12-31 00:00:00
+                            2,2021-12-31 00:00:01
+                            """
+
+    prediction_times_str2 = """dw_ek_borger,timestamp,
+                            2,2021-12-31 00:00:00
+                            2,2021-12-31 00:00:01
+                            1,2021-12-31 00:00:00
+                            1,2021-12-31 00:00:01
+                            """
+
+    prediction_times_df = str_to_df(prediction_times_str)
+    prediction_times_df2 = str_to_df(prediction_times_str2)
+
+    flattened_dataset1 = FlattenedDataset(
+        prediction_times_df=prediction_times_df,
+        timestamp_col_name="timestamp",
+        id_col_name="dw_ek_borger",
+        n_workers=1,
+    )
+
+    flattened_dataset2 = FlattenedDataset(
+        prediction_times_df=prediction_times_df2,
+        timestamp_col_name="timestamp",
+        id_col_name="dw_ek_borger",
+        n_workers=1,
+    )
+
+    predictor_list = create_feature_combinations(
+        [
+            {
+                "predictor_df": "predictor_df",
+                "lookbehind_days": [1, 2, 3, 4],
+                "resolve_multiple": "max",
+                "fallback": 0,
+                "source_values_col_name": "val",
+            },
+        ]
+    )
+
+    predictor_list2 = create_feature_combinations(
+        [
+            {
+                "predictor_df": "predictor_df",
+                "lookbehind_days": [1, 2, 3, 4],
+                "resolve_multiple": "max",
+                "fallback": 0,
+                "source_values_col_name": "val",
+            },
+        ]
+    )
+
+    predictor_str = """dw_ek_borger,timestamp,val,
+                        1,2021-12-30 00:00:01, 1
+                        1,2021-12-29 00:00:02, 2
+                        2,2021-12-30 00:00:01, 1
+                        2,2021-12-29 00:00:02, 2
+                        """
+
+    predictor_df = str_to_df(predictor_str)
+
+    flattened_dataset1.add_predictors_from_list_of_argument_dictionaries(
+        predictor_list=predictor_list,
+        predictor_dfs_dict={"predictor_df": predictor_df},
+    )
+
+    flattened_dataset2.add_predictors_from_list_of_argument_dictionaries(
+        predictor_list=predictor_list2,
+        predictor_dfs_dict={"predictor_df": predictor_df},
+    )
+
+    # We don't care about indeces. Sort to match the ordering.
+    assert_frame_equal(
+        flattened_dataset1.df.sort_values(["dw_ek_borger", "timestamp"]).reset_index(
+            drop=True
+        ),
+        flattened_dataset2.df.sort_values(["dw_ek_borger", "timestamp"]).reset_index(
+            drop=True
+        ),
+        check_index_type=False,
+        check_like=True,
+    )
