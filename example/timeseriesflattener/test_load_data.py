7ff3601e5d3839ae93d1b65f426a5542adc3ff9f
Martin Bernstorff <ryqiem@gmail.com>
2022-04-27 13:33:07 +0200

    Split into loaders module

--- example/timeseriesflattener/test_load_data.py
+++ example/timeseriesflattener/test_load_data.py
@@ -1,8 +1,8 @@
-from load_data import LoadData
+from loaders.medications import LoadMedications
 from wasabi import msg
 
 if __name__ == "__main__":
-    df = LoadData.medication(atc_str="A10")
+    df = LoadMedications.medication(atc_str="A10")
 
     msg.info(f"Columns: {df.columns}")
 
