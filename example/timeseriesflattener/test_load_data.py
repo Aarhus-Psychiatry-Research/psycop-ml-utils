89dfb52619c63f8b0998c534324bd4e719e57a42
Martin Bernstorff <martinbernstorff@gmail.com>
2022-04-27 14:23:06 +0200

    Add depth, refactor (#62, closes #60)

--- example/timeseriesflattener/test_load_data.py
+++ example/timeseriesflattener/test_load_data.py
@@ -2,7 +2,7 @@ from loaders.medications import LoadMedications
 from wasabi import msg
 
 if __name__ == "__main__":
-    df = LoadMedications.medication(atc_str="A10")
+    df = LoadMedications.medication(atc_str="A10", depth=4)
 
     msg.info(f"Columns: {df.columns}")
 
