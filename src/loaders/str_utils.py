2fd4618f9da8332789c98d8f820702fad1dcdb34
Martin Bernstorff <ryqiem@gmail.com>
2022-04-27 15:09:19 +0200

    Improve docstrings.

--- src/loaders/str_utils.py
+++ src/loaders/str_utils.py
@@ -5,6 +5,17 @@ from wasabi import msg
 def create_cols_for_unique_vals_at_depth(
     df: DataFrame, source_col_name: str, depth: int
 ) -> DataFrame:
+    """Create a column for each unique value at string depth.
+    E.g. for [E10, E11, E12] at depth 2, create [E1], at depth 3, create [E10, E11, E12].
+
+    Args:
+        df (DataFrame): Input dataframe.
+        source_col_name (str): Column name with the strings in the input dataframe.
+        depth (int): How far down to go in the string, see above documentation.
+
+    Returns:
+        DataFrame
+    """
     df["substr"] = df[source_col_name].str[:depth]
 
     categories = df["substr"].unique().tolist()
