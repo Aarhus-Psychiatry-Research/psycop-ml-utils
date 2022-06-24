4afcacd54f1d8c5f1655b80c1ab99ba276b70195
Martin Bernstorff <ryqiem@gmail.com>
2022-06-24 14:46:12 +0200

    fix: naming of classes and npukode rename

--- example/timeseriesflattener/generate_features_and_write_to_sql.py
+++ example/timeseriesflattener/generate_features_and_write_to_sql.py
@@ -56,8 +56,10 @@ if __name__ == "__main__":
 
     # Predictors
     msg.info("Adding static predictors")
-    flattened_df.add_static_predictor(psycopmlutils.loaders.LoadDemographics.male())
-    flattened_df.add_age(psycopmlutils.loaders.LoadDemographics.birthdays())
+    flattened_df.add_static_predictor(
+        psycopmlutils.loaders.LoadDemographic.sex_female(),
+    )
+    flattened_df.add_age(psycopmlutils.loaders.LoadDemographic.birthdays())
 
     start_time = time.time()
 
