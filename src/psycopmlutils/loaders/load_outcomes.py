d7d3f721357ea0b9b80a944a62d42a05b25bb6f3
bokajgd <bokajgd@gmail.com>
2022-08-24 09:55:49 +0200

    adding loaders

--- src/psycopmlutils/loaders/load_outcomes.py
+++ src/psycopmlutils/loaders/load_outcomes.py
@@ -38,3 +38,39 @@ class LoadOutcome:
         msg.good("Finished loading any_diabetes event times")
         output = df[["dw_ek_borger", "timestamp", "value"]]
         return output.reset_index(drop=True)
+
+    @data_loaders.register("forced_admissions_indlagt")
+    def forced_admnissions_indlagt():
+
+        df = sql_load(
+            "SELECT dw_ek_borger, timestamp FROM [fct].[psycop_fa_outcomes_all_disorders_tvangsindlaeg_Indlagt_2y_0f_2015-2021]",
+            database="USR_PS_FORSK",
+            chunksize=None,
+        )
+
+        df = df[["dw_ek_borger", "datotid_slut", "six_month"]]
+
+        df.rename(columns={"datotid_slut": "timestamp", "six_month": "value"}, inplace=True)
+        df["timestamp"] = pd.to_datetime(df["timestamp"]).dt.tz_localize(None)
+
+        msg.good("Finished loading forced admissions indlagt predicction times with outcome")
+
+        return df.reset_index(drop=True)
+
+    @data_loaders.register("forced_admissions_ambulant")
+    def forced_admnissions_ammbulant():
+
+        df = sql_load(
+            "SELECT dw_ek_borger, timestamp FROM [fct].[psycop_fa_outcomes_all_disorders_tvangsindlaeg_Ambulant_2y_0f_2015-2021]",
+            database="USR_PS_FORSK",
+            chunksize=None,
+        )
+
+        df = df[["dw_ek_borger", "datotid_predict", "six_month"]]
+
+        df.rename(columns={"datotid_predict": "timestamp", "six_month": "value"}, inplace=True)
+        df["timestamp"] = pd.to_datetime(df["timestamp"]).dt.tz_localize(None)
+
+        msg.good("Finished loading forced admissions ambulant predicction times with outcome")
+
+        return df.reset_index(drop=True)
