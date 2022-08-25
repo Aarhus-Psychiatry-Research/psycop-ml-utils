8b1b4f14d6e9ec340590aa8f1b596c072319994b
Lasse <lasseh0310@gmail.com>
2022-08-25 11:22:21 +0200

    refactor: add options to run each check individually

--- src/psycopmlutils/timeseriesflattener/data_integrity.py
+++ src/psycopmlutils/timeseriesflattener/data_integrity.py
@@ -14,11 +14,16 @@ from deepchecks.tabular.checks import (
     NewLabelTrainTest,
     TrainTestLabelDrift,
 )
-from deepchecks.tabular.suites import data_integrity, train_test_validation
+from deepchecks.tabular.suites import data_integrity
 from wasabi import Printer
 
 
-def check_feature_sets_dir(path: Path, nrows: Optional[int] = None) -> None:
+def check_feature_sets_dir(
+    path: Path,
+    nrows: Optional[int] = None,
+    check_data_integrity: Optional[bool] = True,
+    check_split_integrity: Optional[bool] = True,
+) -> None:
     """Runs Deepcheck data integrity and train/val/test drift checks for a
     given directory containing train/val/test files. Expects the directory to
     contain 3 csv files, where the files have ["train", "val", "test"]
@@ -30,9 +35,10 @@ def check_feature_sets_dir(path: Path, nrows: Optional[int] = None) -> None:
         path (Path): Path to a directory containing train/val/test files
         nrows (Optional[int]): Whether to only load a subset of the data.
         Should only be used for debugging.
+        check_data_integrity (Optional[bool]): Whether to run data integrity checks
+        check_split_integrity (Optional[bool]): Whether to run train/val/test drift checks
     """
     msg = Printer(timestamp=True)
-    msg.info("Running data integrity checks...")
 
     out_dir = path / "deepchecks"
     if not out_dir.exists():
@@ -46,114 +52,121 @@ def check_feature_sets_dir(path: Path, nrows: Optional[int] = None) -> None:
     #### DATA INTEGRITY
     ###################
 
-    # Only running data integrity checks on the training set to reduce the
-    # chance of any form of peaking at the test set
-    train_predictors, train_outcomes = load_split_predictors_and_outcomes(
-        path=path,
-        split="train",
-        include_id=False,
-        nrows=nrows,
-    )
-    ds = Dataset(df=train_predictors, datetime_name="timestamp")
-
-    # Running checks that do not require a label
-    integ_suite = data_integrity()
-    suite_results = integ_suite.run(ds)
-    suite_results.save_as_html(str(out_dir / "data_integrity.html"))
-
-    # Running checks that require a label for each outcome
-    label_checks = label_integrity_checks()
-    for outcome_column in train_outcomes.columns:
-        msg.info(f"Running data integrity for {outcome_column}")
-        ds = Dataset(
-            df=train_predictors,
-            datetime_name="timestamp",
-            label=train_outcomes[outcome_column],
+    if check_data_integrity:
+        msg.info("Running data integrity checks...")
+        # Only running data integrity checks on the training set to reduce the
+        # chance of any form of peaking at the test set
+        train_predictors, train_outcomes = load_split_predictors_and_outcomes(
+            path=path,
+            split="train",
+            include_id=False,
+            nrows=nrows,
         )
-        suite_results = label_checks.run(ds)
-        suite_results.save_as_html(
-            str(outcome_checks_dir / f"{outcome_column}_check.html"),
-        )
+        ds = Dataset(df=train_predictors, datetime_name="timestamp")
 
-    msg.good("Finshed data integrity checks!")
+        # Running checks that do not require a label
+        integ_suite = data_integrity()
+        suite_results = integ_suite.run(ds)
+        suite_results.save_as_html(str(out_dir / "data_integrity.html"))
+
+        # Running checks that require a label for each outcome
+        label_checks = label_integrity_checks()
+        for outcome_column in train_outcomes.columns:
+            msg.info(f"Running data integrity for {outcome_column}")
+            ds = Dataset(
+                df=train_predictors,
+                datetime_name="timestamp",
+                label=train_outcomes[outcome_column],
+            )
+            suite_results = label_checks.run(ds)
+            suite_results.save_as_html(
+                str(outcome_checks_dir / f"{outcome_column}_check.html"),
+            )
+
+        msg.good("Finshed data integrity checks!")
 
     #####################
     #### SPLIT VALIDATION
     #####################
+    if check_split_integrity:
+        msg.info("Running split validation...")
+        # Running data validation checks on train/val and train/test splits that do not
+        # require a label
+        validation_suite = custom_train_test_validation()
 
-    msg.info("Running split validation...")
-    # Running data validation checks on train/val and train/test splits that do not
-    # require a label
-    validation_suite = custom_train_test_validation()
+        train_predictors, train_outcomes = load_split_predictors_and_outcomes(
+            path=path,
+            split="train",
+            include_id=True,
+            nrows=nrows,
+        )
+        val_predictors, val_outcomes = load_split_predictors_and_outcomes(
+            path=path,
+            split="val",
+            include_id=True,
+            nrows=nrows,
+        )
+        test_predictors, test_outcomes = load_split_predictors_and_outcomes(
+            path=path,
+            split="test",
+            include_id=True,
+            nrows=nrows,
+        )
 
-    train_predictors, train_outcomes = load_split_predictors_and_outcomes(
-        path=path,
-        split="train",
-        include_id=True,
-        nrows=nrows,
-    )
-    val_predictors, val_outcomes = load_split_predictors_and_outcomes(
-        path=path,
-        split="val",
-        include_id=True,
-        nrows=nrows,
-    )
-    test_predictors, test_outcomes = load_split_predictors_and_outcomes(
-        path=path,
-        split="test",
-        include_id=True,
-        nrows=nrows,
-    )
+        train_ds = Dataset(
+            train_predictors,
+            index_name="dw_ek_borger",
+            datetime_name="timestamp",
+        )
+        val_ds = Dataset(
+            val_predictors,
+            index_name="dw_ek_borger",
+            datetime_name="timestamp",
+        )
+        test_ds = Dataset(
+            test_predictors,
+            index_name="dw_ek_borger",
+            datetime_name="timestamp",
+        )
+        suite_results = validation_suite.run(train_ds, val_ds)
+        suite_results.save_as_html(str(out_dir / "train_val_integrity.html"))
+        suite_results = validation_suite.run(train_ds, test_ds)
+        suite_results.save_as_html(str(out_dir / "train_test_integrity.html"))
 
-    train_ds = Dataset(
-        train_predictors,
-        index_name="dw_ek_borger",
-        datetime_name="timestamp",
-    )
-    val_ds = Dataset(
-        val_predictors,
-        index_name="dw_ek_borger",
-        datetime_name="timestamp",
-    )
-    test_ds = Dataset(
-        test_predictors,
-        index_name="dw_ek_borger",
-        datetime_name="timestamp",
-    )
-    suite_results = validation_suite.run(train_ds, val_ds)
-    suite_results.save_as_html(str(out_dir / "train_val_integrity.html"))
-    suite_results = validation_suite.run(train_ds, test_ds)
-    suite_results.save_as_html(str(out_dir / "train_test_integrity.html"))
+        # Running checks that require a label for each outcome
+        label_split_check = label_split_checks()
 
-    # Running checks that require a label for each outcome
-    label_split_check = label_split_checks()
+        split_dict = {
+            "val": {"predictors": val_predictors, "outcomes": val_outcomes},
+            "test": {"predictors": test_predictors, "outcomes": test_outcomes},
+        }
 
-    split_dict = {
-        "val": {"predictors": val_predictors, "outcomes": val_outcomes},
-        "test": {"predictors": test_predictors, "outcomes": test_outcomes},
-    }
+        for split in split_dict:
+            for outcome_column in train_outcomes:
+                msg.info(
+                    f"Running split validation for train/{split} and {outcome_column}"
+                )
+                train_ds = Dataset(
+                    df=train_predictors,
+                    index_name="dw_ek_borger",
+                    datetime_name="timestamp",
+                    label=train_outcomes[outcome_column],
+                )
+                split_ds = Dataset(
+                    df=split["predictors"],
+                    index_name="dw_ek_borger",
+                    datetime_name="timestamp",
+                    label=split["outcomes"][outcome_column],
+                )
+                suite_results = label_split_check.run(train_ds, split_ds)
+                suite_results.save_as_html(
+                    str(
+                        outcome_checks_dir
+                        / f"train_{split}_{outcome_column}_check.html"
+                    ),
+                )
 
-    for split in split_dict:
-        for outcome_column in train_outcomes:
-            msg.info(f"Running split validation for train/{split} and {outcome_column}")
-            train_ds = Dataset(
-                df=train_predictors,
-                index_name="dw_ek_borger",
-                datetime_name="timestamp",
-                label=train_outcomes[outcome_column],
-            )
-            split_ds = Dataset(
-                df=split["predictors"],
-                index_name="dw_ek_borger",
-                datetime_name="timestamp",
-                label=split["outcomes"][outcome_column],
-            )
-            suite_results = label_split_check.run(train_ds, split_ds)
-            suite_results.save_as_html(
-                str(outcome_checks_dir / f"train_{split}_{outcome_column}_check.html"),
-            )
-
-    msg.good(f"All data checks done! Saved to {out_dir}")
+        msg.good(f"All data checks done! Saved to {out_dir}")
 
 
 def label_integrity_checks() -> Suite:
