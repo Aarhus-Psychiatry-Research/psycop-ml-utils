ac56f0e3d05a58c69bfad7904d005246ca6f4c8e
bokajgd <bokajgd@gmail.com>
2022-08-26 12:51:51 +0200

    renaming script and small corrections

--- example/loaders/load_forced_admissions_dfs_with_prediction_times_and_outcome.py
+++ example/loaders/load_forced_admissions_dfs_with_prediction_times_and_outcome.py
@@ -5,9 +5,9 @@ from psycopmlutils.loaders.sql_load import sql_load
 from psycopmlutils.utils import data_loaders
 
 
-class LoadForce:
-    @data_loaders.register("forced_admissions_indlagt")
-    def forced_admissions_indlagt():
+class LoadForced:
+    @data_loaders.register("forced_admissions_admissions")
+    def forced_admissions_admissions():
 
         df = sql_load(
             "SELECT * FROM [fct].[psycop_fa_outcomes_all_disorders_tvangsindlaeg_Indlagt_2y_0f_2015-2021]",
@@ -24,16 +24,16 @@ class LoadForce:
             },
             inplace=True,
         )
-        df["timestamp"] = pd.to_datetime(df["timestamp"]).dt.tz_localize(None)
+        df["timestamp"] = pd.to_datetime(df["timestamp"])
 
         msg.good(
-            "Finished loading forced admissions indlagt predicction times with outcome"
+            "Finished loading data frame for forced admissions with prediction times and outcome for all outpatient visits",
         )
 
         return df.reset_index(drop=True)
 
-    @data_loaders.register("forced_admissions_ambulant")
-    def forced_admnissions_ambulant():
+    @data_loaders.register("forced_admissions_outpatient_visits")
+    def forced_admnissions_outpatient_visits():
 
         df = sql_load(
             "SELECT * FROM [fct].[psycop_fa_outcomes_all_disorders_tvangsindlaeg_Ambulant_2y_0f_2015-2021]",
@@ -50,16 +50,16 @@ class LoadForce:
             },
             inplace=True,
         )
-        df["timestamp"] = pd.to_datetime(df["timestamp"]).dt.tz_localize(None)
+        df["timestamp"] = pd.to_datetime(df["timestamp"])
 
         msg.good(
-            "Finished loading forced admissions ambulant predicction times with outcome"
+            "Finished loading data frame for forced admissions with prediction times and outcome for all admissions",
         )
 
         return df.reset_index(drop=True)
 
 
 if __name__ == "__main__":
-    df = LoadForce.forced_admissions_indlagt()
+    df = LoadForced.forced_admissions_admissions()
 
     pass
