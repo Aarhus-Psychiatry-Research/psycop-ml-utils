32b9c7f54fa3d9066d12ab5865e155e675078531
Martin Bernstorff <martinbernstorff@gmail.com>
2022-09-01 11:20:22 +0200

    feat: add duplicates and nan checks

--- src/psycopmlutils/loaders/raw/check_feature_combination_formatting.py
+++ src/psycopmlutils/loaders/raw/check_feature_combination_formatting.py
@@ -7,15 +7,17 @@ from psycopmlutils.utils import data_loaders
 
 def check_feature_combinations_return_correct_formatting(
     predictor_dict_list: List[Dict[str, Union[str, float, int]]],
-    n: Optional[int] = 100,
+    n: Optional[int] = 1_000,
     required_columns: Optional[List[str]] = ["dw_ek_borger", "timestamp", "value"],
+    subset_duplicates_columns: Optional[List[str]] = ["dw_ek_borger", "timestamp"],
 ):
     """Test that all predictor_dfs in predictor_list return a valid df.
 
     Args:
         predictor_dict_list (Dict[str, Union[str, float, int]]): List of dictionaries where the key predictor_df maps to an SQL database.
-        n (int, optional): Number of rows to test. Defaults to 100.
+        n (int, optional): Number of rows to test. Defaults to 1_000.
         required_columns (List[str], optional): List of required columns. Defaults to ["dw_ek_borger", "timestamp", "value"].
+        subset_duplicates_columns (List[str], optional): List of columns to subset on when checking for duplicates. Defaults to ["dw_ek_borger", "timestamp"].
     """
     msg = Printer(timestamp=True)
 
@@ -29,9 +31,10 @@ def check_feature_combinations_return_correct_formatting(
 
     loader_fns_dict = data_loaders.get_all()
 
-    failed = []
+    failure_dicts = []
 
     for i, predictor_df_str in enumerate(predictor_df_vals):
+        # Check that it returns a dataframe
         try:
             df = loader_fns_dict[predictor_df_str](n=n)
         except KeyError:
@@ -44,26 +47,41 @@ def check_feature_combinations_return_correct_formatting(
 
         prefix = f"{i}/{len(predictor_df_vals)} {predictor_df_str}:"
 
+        # Check that the dataframe has a meaningful length
         if df.shape[0] == 0:
             source_failures.append("No rows returned")
 
+        # Check that the dataframe has the required columns
         for col in required_columns:
             if col not in df.columns:
-                source_failures.append(f"{col} not in columns")
+                source_failures.append(f"{col}: not in columns")
 
+                # Check that columns are correctly formatted
                 if "timestamp" in col:
                     # Check that column has a valid datetime format
                     if col.dtype not in ("datetime64[ns]"):
-                        source_failures.append(f"{col} has invalid datetime format")
+                        source_failures.append(f"{col}: invalid datetime format")
 
+            # Check for NaN in cols
+            n_na = df[col].isna().sum()
+
+            if n_na > 0:
+                source_failures.append(f"{col}: {n_na} NaN")
+
+        # Check for duplicates
+        if df.duplicated(subset=subset_duplicates_columns).any():
+            source_failures.append(f"Duplicates found on {subset_duplicates_columns}")
+
+        # Return errors
         if len(source_failures) != 0:
-            failed.append({predictor_df_str["predictor_df"]: source_failures})
+            failure_dicts.append({predictor_df_str["predictor_df"]: source_failures})
+            msg.fail(f"{prefix} failed, errors: {source_failures}")
         else:
             msg.good(f"{prefix} Conforms to criteria")
 
-    if not failed:
+    if not failure_dicts:
         msg.good(
             f"Checked {len(predictor_df_vals)} predictor_dfs, all returned appropriate dfs",
         )
     else:
-        raise ValueError(f"{failed} all returned empty dfs")
+        raise ValueError(f"{failure_dicts}")
