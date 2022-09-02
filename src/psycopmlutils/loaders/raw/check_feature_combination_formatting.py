2bf7d2df766c00ca176d67fb109120ba88bbaf03
Martin Bernstorff <martinbernstorff@gmail.com>
2022-09-02 09:35:56 +0200

    refactor: move df validation out of check_feature_combinations_return_correct_dfs

--- src/psycopmlutils/loaders/raw/check_feature_combination_formatting.py
+++ src/psycopmlutils/loaders/raw/check_feature_combination_formatting.py
@@ -1,5 +1,6 @@
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Union
 
+import pandas as pd
 from wasabi import Printer
 
 from psycopmlutils.utils import data_loaders
@@ -7,17 +8,17 @@ from psycopmlutils.utils import data_loaders
 
 def check_feature_combinations_return_correct_dfs(
     predictor_dict_list: List[Dict[str, Union[str, float, int]]],
-    n: Optional[int] = 1_000,
-    required_columns: Optional[List[str]] = ["dw_ek_borger", "timestamp", "value"],
-    subset_duplicates_columns: Optional[List[str]] = ["dw_ek_borger", "timestamp"],
+    n: int = 1_000,
+    required_columns: List[str] = ["dw_ek_borger", "timestamp", "value"],
+    subset_duplicates_columns: List[str] = ["dw_ek_borger", "timestamp"],
 ):
     """Test that all predictor_dfs in predictor_list return a valid df.
 
     Args:
         predictor_dict_list (Dict[str, Union[str, float, int]]): List of dictionaries where the key predictor_df maps to an SQL database.
-        n (int, optional): Number of rows to test. Defaults to 1_000.
-        required_columns (List[str], optional): List of required columns. Defaults to ["dw_ek_borger", "timestamp", "value"].
-        subset_duplicates_columns (List[str], optional): List of columns to subset on when checking for duplicates. Defaults to ["dw_ek_borger", "timestamp"].
+        n (int): Number of rows to test. Defaults to 1_000.
+        required_columns (List[str]): List of required columns. Defaults to ["dw_ek_borger", "timestamp", "value"].
+        subset_duplicates_columns (List[str]): List of columns to subset on when checking for duplicates. Defaults to ["dw_ek_borger", "timestamp"].
     """
     msg = Printer(timestamp=True)
 
@@ -52,46 +53,14 @@ def check_feature_combinations_return_correct_dfs(
             )
             continue
 
-        source_failures = []
-
         prefix = f"{i+1}/{len(unique_subset_dicts)} {d['predictor_df']}:"
 
-        # Check that the dataframe has a meaningful length
-        if df.shape[0] == 0:
-            source_failures.append("No rows returned")
-
-        # Check that the dataframe has the required columns
-        for col in required_columns:
-            if col not in df.columns:
-                source_failures.append(f"{col}: not in columns")
-
-                # Check that columns are correctly formatted
-                if "timestamp" in col:
-                    # Check that column has a valid datetime format
-                    if col.dtype not in ("datetime64[ns]"):
-                        source_failures.append(f"{col}: invalid datetime format")
-
-            # Check for NaN in cols
-            na_prop = round(df[col].isna().sum() / df.shape[0], 2)
-
-            if na_prop > 0:
-                if col != "value":
-                    source_failures.append(f"{col}: {na_prop}% NaN")
-                else:
-                    d["allowed_nan_value_prop"] = (
-                        0.0
-                        if d["allowed_nan_value_prop"] is not None
-                        else d["allowed_nan_value_prop"]
-                    )
-
-                    if na_prop > d["allowed_nan_value_prop"]:
-                        source_failures.append(
-                            f"{col}: {na_prop}% NaN (allowed {d['allowed_nan_value_prop']}%)",
-                        )
-
-        # Check for duplicates
-        if df.duplicated(subset=subset_duplicates_columns).any():
-            source_failures.append(f"Duplicates found on {subset_duplicates_columns}")
+        source_failures, prefix = check_raw_df(
+            df=df,
+            required_columns=required_columns,
+            subset_duplicates_columns=subset_duplicates_columns,
+            allowed_nan_value_prop=d["allowed_nan_value_prop"],
+        )
 
         # Return errors
         if len(source_failures) != 0:
@@ -106,3 +75,57 @@ def check_feature_combinations_return_correct_dfs(
         )
     else:
         raise ValueError(f"{failure_dicts}")
+
+
+def check_raw_df(
+    df: pd.DataFrame,
+    required_columns: List[str],
+    subset_duplicates_columns: List[str],
+    allowed_nan_value_prop: float = 0.0,
+) -> List[str]:
+    """Check that the raw df conforms to the required format and doesn't
+    contain duplicates or missing values.
+
+    Args:
+        df (pd.DataFrame): Dataframe to check.
+        required_columns (List[str]): List of required columns. Defaults to ["dw_ek_borger", "timestamp", "value"].
+        subset_duplicates_columns (List[str]): List of columns to subset on when checking for duplicates. Defaults to ["dw_ek_borger", "timestamp"].
+        allowed_nan_value_prop (float): Allowed proportion of missing values. Defaults to 0.0.
+
+    Returns:
+        List[str]: List of errors.
+    """
+    source_failures = []
+
+    # Check that the dataframe has a meaningful length
+    if df.shape[0] == 0:
+        source_failures.append("No rows returned")
+
+        # Check that the dataframe has the required columns
+    for col in required_columns:
+        if col not in df.columns:
+            source_failures.append(f"{col}: not in columns")
+
+            # Check that columns are correctly formatted
+            if "timestamp" in col:
+                # Check that column has a valid datetime format
+                if col.dtype not in ("datetime64[ns]"):
+                    source_failures.append(f"{col}: invalid datetime format")
+
+            # Check for NaN in cols
+        na_prop = round(df[col].isna().sum() / df.shape[0], 2)
+
+        if na_prop > 0:
+            if col != "value":
+                source_failures.append(f"{col}: {na_prop}% NaN")
+            else:
+                if na_prop > allowed_nan_value_prop:
+                    source_failures.append(
+                        f"{col}: {na_prop}% NaN (allowed {allowed_nan_value_prop}%)",
+                    )
+
+    # Check for duplicates
+    if df.duplicated(subset=subset_duplicates_columns).any():
+        source_failures.append(f"Duplicates found on {subset_duplicates_columns}")
+
+    return source_failures
