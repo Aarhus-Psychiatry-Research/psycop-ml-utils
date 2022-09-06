90b75d9c32dfe7fd5be6f52e499e0d4c87f9cb26
Lasse <lasseh0310@gmail.com>
2022-09-06 12:25:41 +0200

    init skeleton

---
+++ src/psycopmlutils/loaders/raw/load_text.py
@@ -0,0 +1,38 @@
+# from typing import List, Optional
+
+import pandas as pd
+
+# from wasabi import msg
+
+# from psycopmlutils.loaders.raw.sql_load import sql_load
+# from psycopmlutils.utils import data_loaders
+
+# Load only 1 note type
+
+# Load specific note types
+
+# Load all
+
+# Featurize using tf-idf (pretrained from some path)
+# Featurize using doc embedding (from some HF path)
+
+
+class LoadText:
+    def load_note_type() -> pd.DataFrame:
+        # FOR_SFI_fritekst_resultat_udfoert_i_psykiatrien_aendret_2011_inkl_2021_feb2022
+        pass
+
+    def load_note_types() -> pd.DataFrame:
+        pass
+
+    def load_all_notes() -> pd.DataFrame:
+        pass
+
+    def featurize_text() -> pd.DataFrame:
+        pass
+
+    def _tfidf_featurize() -> pd.DataFrame:
+        pass
+
+    def _huggingface_featurize() -> pd.DataFrame:
+        pass
