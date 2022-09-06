ff41b47cfc6a450f7e28fcdd3655a644922e99be
Martin Bernstorff <ryqiem@gmail.com>
2022-09-06 11:25:53 +0200

    fix: don't coerce when all values have been coerced

--- src/psycopmlutils/loaders/non_numerical_coercer.py
+++ src/psycopmlutils/loaders/non_numerical_coercer.py
@@ -29,7 +29,13 @@ def multiply_inequalities_in_df(
     in_eqs = sorted(ineq2mult.keys(), key=len, reverse=True)
 
     for in_eq in in_eqs:
-        starts_with_ineq_idxs = df[col_to_multiply].str.startswith(in_eq).fillna(False)
+        try:
+            starts_with_ineq_idxs = (
+                df[col_to_multiply].str.startswith(in_eq).fillna(False)
+            )
+        except AttributeError:
+            # If the column is no longer a string (i.e. all values have been coerced), continue
+            continue
 
         df.loc[starts_with_ineq_idxs, col_to_multiply] = (
             df.loc[starts_with_ineq_idxs, col_to_multiply]
@@ -37,7 +43,7 @@ def multiply_inequalities_in_df(
             .str.replace(",", ".")  # Replace comma with dot for float conversion.
             .astype(float)
             .mul(ineq2mult[in_eq])
-            .round(0)
+            .round(2)
         )
 
     # Convert col_to_multiply dtype to float
