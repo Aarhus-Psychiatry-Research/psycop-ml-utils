a127328dc25a4a42c8f02e1b89bd1acc81276ef5
bokajgd <bokajgd@gmail.com>
2022-09-06 14:58:37 +0200

    Fix: coercion loaders sql query Fixes #193

--- example/loaders/load_coercion.py
+++ example/loaders/load_coercion.py
@@ -2,7 +2,7 @@ from psycopmlutils.loaders.raw.load_coercion import LoadCoercion
 
 if __name__ == "__main__":
     df = LoadCoercion.coercion_duration(n=100)
-    farlighed = LoadCoercion.coercion_farlighed(n=20)
+    farlighed = LoadCoercion.farlighed(n=20)
     bælte = LoadCoercion.bælte(n=100)
 
     pass
