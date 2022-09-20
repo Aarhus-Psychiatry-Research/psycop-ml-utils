6c7e94f9e58c9b06a42532f00ff0da69129a14cc
erikperfalk <erik.perfalk@gmail.com>
2022-09-20 10:06:49 +0200

    updated data loaders coercion

--- example/loaders/load_coercion.py
+++ example/loaders/load_coercion.py
@@ -5,10 +5,10 @@
 import psycopmlutils.loaders.raw.load_coercion as c
 
 if __name__ == "__main__":
-    df = c.load_corcioncoercion_duration(n=100)
-    farlighed = c.farlighed(n=20)
-    bælte = c.bælte(n=100)
-
+    df = c.coercion_duration(n_rows=100)
+    farlighed = c.farlighed(n_rows=20)
+    bælte = c.bælte(n_rows=100)
+ 
 
 __all__ = [
     "bælte",
