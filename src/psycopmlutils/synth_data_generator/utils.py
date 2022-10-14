55e261934a8c6894328e495c366d7c0c8533f3e3
Martin Bernstorff <martinbernstorff@gmail.com>
2022-10-14 11:00:32 +0200

    fix: errors in synth data generation and add as test

--- src/psycopmlutils/synth_data_generator/utils.py
+++ src/psycopmlutils/synth_data_generator/utils.py
@@ -1,16 +1,22 @@
 """Utilites for generating synthetic data."""
 
+from typing import Optional
+
 import numpy as np
 import pandas as pd
 
 
-def replace_vals_with_na(df: pd.DataFrame, na_prob: float, na_ignore_cols: list[str]):
+def replace_vals_with_na(
+    df: pd.DataFrame,
+    na_prob: float,
+    na_ignore_cols: Optional[list[str]] = None,
+):
     """Replace values with NAs.
 
     Args:
         df (pd.DataFrame): The dataframe to replace values in.
         na_prob (float): The probability of replacing a value with NA.
-        na_ignore_cols (list[str]): The columns to ignore when replacing values.
+        na_ignore_cols (Optional[list[str]]): The columns to ignore when replacing values.
 
     Returns:
         pd.DataFrame: The dataframe with values replaced with NAs.
@@ -20,7 +26,7 @@ def replace_vals_with_na(df: pd.DataFrame, na_prob: float, na_ignore_cols: list[
 
     # For all columns in df.columns if column is not in na_ignore_cols
     for col in df.columns:
-        if col not in na_ignore_cols:
+        if na_ignore_cols and col not in na_ignore_cols:
             df[col] = df_[col]
 
     return df
---
+++ src/psycopmlutils/utils.py
@@ -0,0 +1,4 @@
+"""Utilities for psycopmlutils."""
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).parent.parent.parent
